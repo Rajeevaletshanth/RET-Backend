ee5fdad5ca02a1b5600ae3d362b5ca4989b54d3f
Rajeevaletshanth <vjvfc2k16@gmail.com>
2025-10-02 13:54:04 +0530

    2nd commit

--- src/utils/jwt.util.ts
+++ src/utils/jwt.util.ts
@@ -4,23 +4,23 @@ import type { JWTPayload } from "../types"
 
 export class JWTUtil {
   static generateAccessToken(payload: JWTPayload): string {
-    return jwt.sign(payload, jwtConfig.accessSecret, {
+    return (jwt as any).sign(payload, jwtConfig.accessSecret as string, {
       expiresIn: jwtConfig.accessExpiry,
     })
   }
 
   static generateRefreshToken(payload: JWTPayload): string {
-    return jwt.sign(payload, jwtConfig.refreshSecret, {
+    return (jwt as any).sign(payload, jwtConfig.refreshSecret as string, {
       expiresIn: jwtConfig.refreshExpiry,
     })
   }
 
   static verifyAccessToken(token: string): JWTPayload {
-    return jwt.verify(token, jwtConfig.accessSecret) as JWTPayload
+    return jwt.verify(token, jwtConfig.accessSecret as string) as JWTPayload
   }
 
   static verifyRefreshToken(token: string): JWTPayload {
-    return jwt.verify(token, jwtConfig.refreshSecret) as JWTPayload
+    return jwt.verify(token, jwtConfig.refreshSecret as string) as JWTPayload
   }
 
   static getRefreshTokenExpiry(): Date {
